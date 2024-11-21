4b462d7d26e1663b52c56923c2433618a6140f90
Serebro1 <OtcheskovSemen@yandex.ru>
2024-11-21 16:21:33 +0300

    Final version

--- MyForm.h
+++ MyForm.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <string>
+#include <msclr\marshal_cppstd.h>
+#include "..\mp2-lab3-stack\calc.h"
 namespace CppWinForm1 {
 
 	using namespace System;
@@ -34,6 +36,13 @@ namespace CppWinForm1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ exprTBox;
+	protected:
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ resultLabel;
 
 	private:
 		/// <summary>
@@ -48,12 +57,97 @@ namespace CppWinForm1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->exprTBox = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->resultLabel = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(13, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(201, 24);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Введите выражение:";
+			// 
+			// exprTBox
+			// 
+			this->exprTBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->exprTBox->Location = System::Drawing::Point(17, 59);
+			this->exprTBox->Name = L"exprTBox";
+			this->exprTBox->Size = System::Drawing::Size(197, 29);
+			this->exprTBox->TabIndex = 1;
+			this->exprTBox->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &MyForm::exprTBox_KeyPress);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(245, 56);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(111, 37);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Calculate";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(13, 97);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(108, 24);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Результат:";
+			// 
+			// resultLabel
+			// 
+			this->resultLabel->AutoSize = true;
+			this->resultLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->resultLabel->Location = System::Drawing::Point(127, 97);
+			this->resultLabel->Name = L"resultLabel";
+			this->resultLabel->Size = System::Drawing::Size(0, 24);
+			this->resultLabel->TabIndex = 4;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(381, 130);
+			this->Controls->Add(this->resultLabel);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->exprTBox);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		std::string infix;
+		infix = msclr::interop::marshal_as<std::string>(exprTBox->Text);
+		TCalc calculator;
+		calculator.SetInfix(infix);
+		resultLabel->Text = Convert::ToString(calculator.Calcul());
+	}
+
+
+private: System::Void exprTBox_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if (e->KeyChar == 13) {
+		button1_Click(sender, e);
+	}
+}
+};
 }
